d00f31f407485c3627294ea79ff80bfbcc7c38a6
W3wallace <159574422+W3wallace@users.noreply.github.com>
2024-03-16 12:33:32 -0400

    Prueba 16

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -21,11 +21,11 @@ void exercise_2(double A, double B, double C) {
 }
 
 void exercise_3(int a, int b) {
-  // TODO: YOUR CODE HERE
+
 }
 
 void exercise_4(double n, double a, double b, double x, double y) {
-  // TODO: YOUR CODE HERE
+
 }
 
 void exercise_5(char character) {
