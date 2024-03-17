26f8921a1153e279cd033ab41f909ceba12730c8
W3wallace <159574422+W3wallace@users.noreply.github.com>
2024-03-16 22:35:44 -0400

    Final prueba 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -18,23 +18,23 @@ void exercise_1(string s1, string s2, string s3) {
   }
 
 void exercise_2(double A, double B, double C) {
-   double D = B * B - 4 * A * C;
+    double D = B * B - 4 * A * C;
    if (D > 0 && A != 0)
    {
        double d_sqrt = sqrt (D);
-       double x1 = (-B + d_sqrt) / (2A);
-       double x2 = (-B - d_sqrt) / (2A); 
+       double x1 = (-B + d_sqrt) / (2*A);
+       double x2 = (-B - d_sqrt) / (2*A); 
        cout<<x1<<" "<<x2<<endl;
    }
    else if (D==0 && A != 0)
    {
    double d_sqrt = sqrt (D);
-   double x1 = (-B + d_sqrt) / (2A); 
+   double x1 = (-B + d_sqrt) / (2*A); 
    cout<<x1<<endl;
    }
    else if (A==0&& B!=0)
    { 
-       cout<< (-1(C))/B <<endl;
+       cout<< (-1*(C))/B <<endl;
    }
    else if (A!=0 && B==0)
    {
@@ -43,7 +43,7 @@ void exercise_2(double A, double B, double C) {
    else
    {
        cout<<"";
-   }
+   } 
 }
 void exercise_4(double n, double a, double b, double x, double y) { 
     double porc;
@@ -63,7 +63,7 @@ void exercise_4(double n, double a, double b, double x, double y) {
 }
 
 void exercise_5(char character) {
-  nt res = character;
+  int res = character;
   if (res >= 97 && res < 123){
     cout << "lower-case alphabet" << endl;
   }
@@ -74,7 +74,6 @@ void exercise_5(char character) {
     cout << "not an alphabet" << endl;
   }
 }
-
 void exercise_6(int number) {
   if(number == 1){
   cout << "Monday" << endl;
@@ -108,15 +107,13 @@ void exercise_7(double r) {
   {
     cout << 4*3.14* r*r << endl;
   }
-else
+  else
   {
-    cout << "Error: Radius cannot be negative." endl;
+    cout << "Error: Radius cannot be negative." << endl;
   }
 }
-  
-
 void exercise_8(long int seconds) {
- int h;
+int h;
 int m;
 if(seconds < 0){
     cout << "Error: Input seconds cannot be negative." << endl;
@@ -150,28 +147,28 @@ else{
 }
 }
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
-  char caracter1 = s1[0];
+   char caracter1 = s1[0];
   char caracter2 = s2[0];
   char caracter3 = s3[0];
   char caracter4 = s4[0];
   char caracter5 = s5[0];
-  cout <<character 1  <<character 2  <<caracter 3 <<caracter 4 <<caracter 5 << endl;
-  if ( caracter1 == caracter5 ) {
-    result "Hemos encontrado algo";
+  cout << caracter1 << caracter2 << caracter3 << caracter4 << caracter5 << endl;
+  if ( caracter1 == caracter5) {
+    return "Hemos encontrado algo!";
   }
   else {
+    return"Aun sin suerte";
   }
-  return "Aun sin suerte";
 }
 
 int exercise_10(int a, int b) {
-  if (a < 0 && b < 0) {
+   if (a < 0 && b < 0) {
     return 0;
   }
-  int sum - a + b;
-  int digit_count - (sum --0)? 1:
+  int sum = a + b;
+  int digit_count = (sum == 0)? 1:
   (int)log10(abs(sum)) + 1;
-  return sum* digit_count;
+  return sum * digit_count;
 }
 
 string exercise_11(int number) {
